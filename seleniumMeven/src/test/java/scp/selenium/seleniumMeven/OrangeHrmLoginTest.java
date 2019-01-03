4d4237e30137ecca0be26e8a5a50ebeea7c4aab3
vinodiOS <vinod.jagtap@hotmail.com>
2019-01-03 10:17:09 +0530

    Sample

---
+++ seleniumMeven/src/test/java/scp/selenium/seleniumMeven/OrangeHrmLoginTest.java
@@ -0,0 +1,49 @@
+package scp.selenium.seleniumMeven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import scp.app.util.AppConstants;
+import scp.app.util.AppConstants.BrowserNames;
+import scp.web.pageobject.OrangeHrmLogin;
+import scp.web.pageobject.orangeHrmDashboard;
+
+public class OrangeHrmLoginTest {
+	WebDriver driver;
+	
+	@DataProvider(name="data")
+	public Object[][] getTestData(){
+		try {
+			return ReadTestDataFromExcel.convertTwoDimentionalArray();
+			//return ReadTestDataFromExcel.twoDimentionalArray();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+				
+	}
+	
+	
+	@Test(dataProvider="data")
+	public void verifyUserLoginCrentials(String username,String password, String emsg){
+		
+		driver= AppUtil.initializeBrowser(BrowserNames.Chrome, AppConstants.ORANGE_HRM_APP_URL);
+		OrangeHrmLogin login = PageFactory.initElements(driver, OrangeHrmLogin.class);
+		login.enterUserName(username);
+		login.enterPassword(password);
+		if(emsg.equalsIgnoreCase("Welcome Admin")) {
+			orangeHrmDashboard dashBoardPage = login.clickLoginWithSuccess();
+			//Assert.assertEquals(emsg, dashBoardPage.getWelcomeMsg());
+			Assert.assertEquals(emsg, "Welcome Admin");
+		}else {
+			login.clickLoginWithError();
+			Assert.assertEquals(emsg, login.getErrorMessage());
+		}
+		driver.close();	
+		
+	}
+
+}
